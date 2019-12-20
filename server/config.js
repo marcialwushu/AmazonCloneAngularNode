cbcb97622c26f45ef97ea5360142c57af219dfdb
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2019-12-20 15:04:32 -0200

    mongoose conect with MongoDB Atlas in config.js

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database: 'mongodb+srv://<user>:<password>@cluster0-b3xyi.mongodb.net/test?retryWrites=true&w=majority',
+    port: '3030',
+    secret: 'MarcialWushu20191220'
+}
+
+
--- server/server.js
+++ server/server.js
@@ -3,8 +3,29 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const config = require('./config');
+
 const app = express();
+
+mongoose.connect(config.database, (err) => {
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Connected  to the database");
+    }
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+
+app.get('/', (req,res, next) => {
+    res.json({
+        user: 'Cleilson Pereira'
+    });
+});
+
+
+app.listen(config.port, err => {
+    console.log('Magic happens  on port' + config.port);
+});
