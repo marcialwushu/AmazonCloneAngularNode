96d266239d394b2821b5cc33942d508a77c9f435
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-02 18:17:59 -0200

    mongoClient and cors added

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb+srv://<user>:<password>@cluster0-b3xyi.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb+srv://<user>:<>password@cluster0-795dg.mongodb.net/test?retryWrites=true&w=majority',
     port: '3030',
     secret: 'MarcialWushu20191220'
 }
--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const config = require('./config');
 
 const app = express();
 
-mongoose.connect(config.database, (err) => {
-    if(err){
-        console.log(err);
-    }else {
-        console.log("Connected  to the database");
-    }
+const MongoClient = require('mongodb').MongoClient;
+const uri = config.database;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
+
+// mongoose.connect(config.database, (err) => {
+//     if(err){
+//         console.log(err);
+//     }else {
+//         console.log("Connected  to the database");
+//     }
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
